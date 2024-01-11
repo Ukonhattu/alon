8f9ac15e341582462be2ee93422f457e3835ecb4
Daniel Koch <danielkoch@fastmail.com>
2024-01-12 00:49:08 +0200

    ex4

--- src/bin/ex4.rs
+++ src/bin/ex4.rs
@@ -1,5 +1,39 @@
 use std::io;
 
 fn main() {
-    unimplemented!();
+    let mut count = String::new();
+    let mut integers = String::new();
+    io::stdin()
+        .read_line(&mut count)
+        .expect("Unable to read count");
+    io::stdin()
+        .read_line(&mut integers)
+        .expect("Unable to read integers");
+    let input = integers
+        .trim()
+        .split_whitespace()
+        .map(|s| s.parse::<u32>().expect("Parse error"))
+        .collect();
+    collect(input);
+}
+
+fn collect(mut input: Vec<u32>) {
+    let mut count = 0;
+    let mut current;
+    let mut to_remove = Vec::new();
+    while input.is_empty() {
+        current = 0;
+        for i in 0..input.len() {
+            if input[i] >= current {
+                to_remove.push(i);
+                current = input[i];
+            }
+        }
+        to_remove.iter().for_each(|s| {
+            input.remove(*s);
+        });
+        to_remove.clear();
+        count = count + 1;
+    }
+    println!("{}", count)
 }
