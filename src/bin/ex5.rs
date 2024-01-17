3099f8461946e9e3da412855d5913ebfd80f92fe
Daniel Koch <daniel.koch@helsinki.fi>
2024-01-17 22:04:56 +0200

    ex5 done

--- src/bin/ex5.rs
+++ src/bin/ex5.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use std::io;
 
 fn main() {
@@ -5,7 +6,42 @@ fn main() {
     io::stdin()
         .read_line(&mut input)
         .expect("Error reading input");
-    let data = Vec::from(input);
+    let data: Vec<char> = input.trim().chars().collect();
+    generate(data);
 }
 
-fn generate(data: Vec<u8>) {}
+fn generate(data: Vec<char>) {
+    let mut set: HashSet<String> = HashSet::new();
+    let current = String::new();
+    let mut indexes: HashSet<usize> = HashSet::new();
+    for i in 0..data.len() {
+        indexes.insert(i);
+    }
+    recurse(&data, &current, &mut set, &mut indexes);
+    println!("{}", set.len());
+    let mut ans: Vec<String> = set.into_iter().collect();
+    ans.sort();
+    for item in ans {
+        println!("{}", item);
+    }
+}
+
+fn recurse(
+    data: &Vec<char>,
+    current: &String,
+    set: &mut HashSet<String>,
+    indexes: &mut HashSet<usize>,
+) {
+    if current.len() >= data.len() {
+        set.insert(current.clone());
+        return;
+    }
+    let s: String = current.to_string().clone();
+    for &i in indexes.iter() {
+        let mut d: String = s.to_string().clone();
+        let mut new_indexes = indexes.clone();
+        d.push(data[i]);
+        new_indexes.remove(&i);
+        recurse(data, &d, set, &mut new_indexes)
+    }
+}
